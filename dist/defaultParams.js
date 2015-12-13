c67adb2d0af9b44d1b7bd8b3b9433dbc6f9d2c7c
Philip Damra <djheru@gmail.com>
2015-12-13 17:23:43 -0500

    default params and destructuring arrays / objects

--- dist/defaultParams.js
+++ dist/defaultParams.js
@@ -13,3 +13,38 @@ function greet2(greeting) {
 greet2(); // undefined dude
 greet2('ohai');
 greet2('hey', 'holmes');
+
+//param is callable with no default
+function fcn1(cb) {
+  cb();
+}
+//fcn1();//throw error omg "TypeError: cb is not a function"
+
+//param has defalt
+function fcn2() {
+  var cb = arguments.length <= 0 || arguments[0] === undefined ? function () {
+    console.log('ohai you forgot callback');
+  } : arguments[0];
+
+  cb();
+}
+fcn2();
+
+//more cleaner
+function fcn3() {
+  var cb = arguments.length <= 0 || arguments[0] === undefined ? function () {
+    return console.log('ohai this way cleaner');
+  } : arguments[0];
+
+  cb();
+}
+fcn3();
+
+//crazy, man
+var fcn4 = function fcn4() {
+  var cb = arguments.length <= 0 || arguments[0] === undefined ? function () {
+    return console.log('ohai wat ru doing?!?');
+  } : arguments[0];
+  return cb();
+};
+fcn4();
--- src/defaultParams.js
+++ src/defaultParams.js
@@ -11,3 +11,25 @@ function greet2(greeting, name = 'dude') {
 greet2();// undefined dude
 greet2('ohai');
 greet2('hey', 'holmes');
+
+//param is callable with no default
+function fcn1(cb) {
+  cb()
+}
+//fcn1();//throw error omg "TypeError: cb is not a function"
+
+//param has defalt
+function fcn2(cb = function () { console.log('ohai you forgot callback'); }) {
+  cb();
+}
+fcn2();
+
+//more cleaner
+function fcn3(cb = () => console.log('ohai this way cleaner')) {
+  cb();
+}
+fcn3();
+
+//crazy, man
+let fcn4 = (cb = () => console.log('ohai wat ru doing?!?')) => cb();
+fcn4();
